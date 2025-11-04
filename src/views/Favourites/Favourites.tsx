de590c365c8e95f1868e3138ee0354f276c4aee5
OllaWilk <alex.dev.wilk@gmail.com>
2025-11-04 22:43:40 +0100

    chore: initialize React + TypeScript project setup

--- src/views/Favourites/Favourites.tsx
+++
@@ -1,7 +0,0 @@
-export const Favourites = ({}) => {
-  return (
-    <div>
-      <p>Sorry, this page is under construction.</p>
-    </div>
-  );
-};
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,1 @@
-export { CocktailDetails } from './CocktailDetails/CocktailDetails';
-export { CocktailsList } from './CocktailsList/CocktailsList';
-export { Favourites } from './Favourites/Favourites';
 export { NotFound } from './NotFound/NotFound';
