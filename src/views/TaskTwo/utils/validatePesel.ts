7a0dc875685fca064aac17698b100e949325f7f1
OllaWilk <alex.dev.wilk@gmail.com>
2025-11-06 21:26:47 +0100

    refactor: Simplify validatePesel logic and keep consistent string return messages

--- src/views/TaskTwo/utils/validatePesel.ts
+++ src/views/TaskTwo/utils/validatePesel.ts
@@ -1,3 +1,11 @@
-export const validatePesel = (pesel: string): number => {
-  return Number('');
+export const validatePesel = (pesel: string): number | string => {
+  if (pesel.length !== 11 || !pesel) {
+    return 'PESEL must be exactly 11 digits long.';
+  }
+
+  if (!/^\d{11}$/.test(pesel)) {
+    return 'PESEL must contain only digits.';
+  }
+
+  return 'Read for further checks';
 };
