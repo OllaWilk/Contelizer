ccf1e077e9266caf5759f491c1ce4c385cc4e861
OllaWilk <alex.dev.wilk@gmail.com>
2025-11-06 10:19:35 +0100

    feat: add navigation to heme view for better ux

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,11 +1,24 @@
+import { Btn } from "../../components";
+import { NAV_ITEMS } from "../../config/navigation";
+import styles from "./Home.module.scss";
+
 export const Home = () => {
   return (
-    <p>
-      This small application was built as part of a React recruitment task set.
-      Each challenge demonstrates a different aspect of frontend development —
-      from working with text and validation to API integration and data
-      handling. Dive in and explore how each problem has been solved in an
-      interactive and user-friendly way.
-    </p>
+    <div>
+      <p>
+        This small application was built as part of a React recruitment task
+        set. Each challenge demonstrates a different aspect of frontend
+        development — from working with text and validation to API integration
+        and data handling. Dive in and explore how each problem has been solved
+        in an interactive and user-friendly way.
+      </p>
+      <ul className={styles.linksWrap}>
+        {NAV_ITEMS.map((el) => (
+          <li key={el.key}>
+            <Btn text={`Checkout  ${el.label}`} url={el.to} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
