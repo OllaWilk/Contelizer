0ae612fb6826ae0c4ce6fe8a76d590802c28f707
OllaWilk <alex.dev.wilk@gmail.com>
2025-11-07 21:00:43 +0100

    refactor: delete unused elements

--- src/views/TaskOne/TaskOne.tsx
+++ src/views/TaskOne/TaskOne.tsx
@@ -1,24 +1,15 @@
-import { useState, useRef } from "react";
-import { UI_TEXTS } from "./content";
-import type { FormData } from "../../types/task-one-types";
-import { validateFile } from "./utils/validateFile";
-import { readTextFile } from "./utils/readTextFile";
-import {
-  Card,
-  ErrorBlock,
-  FormButton,
-  Input,
-  Output,
-  TaskHeader,
-  Btn,
-  LinkBtn,
-} from "../../components";
-import { useFileField } from "./hooks/useFileField";
-import { useMixedText } from "./hooks/useMixedText";
-import styles from "./TaskOne.module.scss";
+import { useState, useRef } from 'react';
+import { UI_TEXTS } from './content';
+import type { FormData } from '../../types/task-one-types';
+import { validateFile } from './utils/validateFile';
+import { readTextFile } from './utils/readTextFile';
+import { Card, ErrorBlock, FormButton, Input, Output, TaskHeader, LinkBtn } from '../../components';
+import { useFileField } from './hooks/useFileField';
+import { useMixedText } from './hooks/useMixedText';
+import styles from './TaskOne.module.scss';
 
 export const TaskOne = () => {
-  const [formData, setFormData] = useState<FormData>({ file: null, text: "" });
+  const [formData, setFormData] = useState<FormData>({ file: null, text: '' });
   const { file, error, onChange, reset, validateBeforeSubmit } = useFileField();
 
   const fileInputRef = useRef<HTMLInputElement | null>(null);
@@ -34,13 +25,13 @@ export const TaskOne = () => {
       setFormData({ file: null, text });
     }
     reset();
-    if (fileInputRef.current) fileInputRef.current.value = "";
+    if (fileInputRef.current) fileInputRef.current.value = '';
   };
 
   const handleReset = () => {
-    setFormData({ file: null, text: "" });
+    setFormData({ file: null, text: '' });
     reset();
-    if (fileInputRef.current) fileInputRef.current.value = "";
+    if (fileInputRef.current) fileInputRef.current.value = '';
   };
 
   const textDisplay = useMixedText(formData.text);
@@ -51,10 +42,7 @@ export const TaskOne = () => {
       <div className={styles.grid}>
         <div className={`${styles.leftColumn}`}>
           <Card className={styles.left}>
-            <form
-              onSubmit={handleSubmit}
-              onReset={() => setFormData({ file: null, text: "" })}
-            >
+            <form onSubmit={handleSubmit} onReset={() => setFormData({ file: null, text: '' })}>
               <h3>{UI_TEXTS.uploadFile}</h3>
               <Input
                 ref={fileInputRef}
@@ -63,23 +51,15 @@ export const TaskOne = () => {
                 accept=".txt,text/plain"
                 onChange={onChange}
               />
-              {error && <ErrorBlock title={"ups"} message={error} />}
+              {error && <ErrorBlock title={'ups'} message={error} />}
               <div className={styles.formActions}>
-                <FormButton
-                  variant="reset"
-                  onClick={handleReset}
-                  text={UI_TEXTS.reset}
-                />
+                <FormButton variant="reset" onClick={handleReset} text={UI_TEXTS.reset} />
                 <FormButton variant="submit" text={UI_TEXTS.submit} />
               </div>
             </form>
           </Card>
           <Card className={styles.preview}>
-            <LinkBtn
-              url="/sample.txt"
-              text={UI_TEXTS.downloadSample}
-              download
-            />
+            <LinkBtn url="/sample.txt" text={UI_TEXTS.downloadSample} download />
           </Card>
         </div>
         <Card className={styles.right}>
--- src/views/TaskThree/TaskThree.tsx
+++ src/views/TaskThree/TaskThree.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useUserQuery } from './hooks/useUserQuery';
 import { usePaginationParams } from './hooks/usePaginationParams';
 import { UI_TEXTS } from '../TaskOne/content';
