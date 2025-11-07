35bd8eedb0a097c6f92a25c06e2b2b72f458f926
OllaWilk <alex.dev.wilk@gmail.com>
2025-11-07 12:34:30 +0100

    feat: display real user data fetched from backend instead of mock data

--- src/views/TaskThree/TaskThree.tsx
+++ src/views/TaskThree/TaskThree.tsx
@@ -1,59 +1,36 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import type { User } from "../../types/task-three-types";
 import { fetchUsers } from "./utils/http";
 import { UI_TEXTS } from "../TaskOne/content";
 import { Btn, Card, InfoRow, TaskHeader } from "../../components";
 import styles from "./TaskThree.module.scss";
 
-const mockupUsers: User[] = [
-  {
-    id: 1,
-    name: "lorem",
-    email: "ipsum",
-    gender: "male",
-    status: "active",
-  },
-  {
-    id: 2,
-    name: "Dolor",
-    email: "setamet",
-    gender: "female",
-    status: "inactive",
-  },
-];
-
 export const TaskThree = () => {
-  const [error, setError] = useState<string | null>(null);
-  useEffect(() => {
-    (async () => {
-      try {
-        const data = await fetchUsers();
-        console.log("DATA", data);
-        console.log("Fetched users:", data);
-      } catch (err: any) {
-        setError(err.message);
-      }
-    })();
-  }, []);
+  const { data, isLoading, error } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  if (isLoading) return <p>Loading cocktails...</p>;
+
+  if (error instanceof Error) return <p>{error.message}</p>;
 
   return (
     <div className={styles.taskThree}>
-      <TaskHeader paragraph={UI_TEXTS.tasktwo} />
-      {error && <p className={styles.error}>Error: {error}</p>}
+      <TaskHeader paragraph={UI_TEXTS.taskthree} />
       <div className={styles.grid}>
-        {mockupUsers.map((user) => (
-          <Card key={user.id}>
-            <TaskHeader paragraph={user.name} />
+        {data?.map(({ id, name, email, gender, status }) => (
+          <Card key={id}>
+            <TaskHeader paragraph={name} />
             {[
-              { label: "Email", value: user.email },
-              { label: "Gender", value: user.gender },
-              { label: "Status", value: user.status },
+              { label: "Email", value: email },
+              { label: "Gender", value: gender },
+              { label: "Status", value: status },
             ].map((item, idx) => (
               <InfoRow key={idx} label={item.label} value={item.value} />
             ))}
             <div className={styles.actions}>
               <Btn text="Edit" className={styles.editBtn} />
-              <Btn text="Delete" className={styles.deleteBtn} />
             </div>
           </Card>
         ))}
